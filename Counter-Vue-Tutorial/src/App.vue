11744a9cd85b615ac196e7f885f853785b7fa885
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2023-02-22 09:30:28 +0300

    создание счетчика на vue

---
+++ Counter-Vue-Tutorial/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import {ref} from "vue";
+
+  const count = ref(0);
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>Текущее значение счетчика</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click=" count++">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: azure;
+    height: 100vh;
+    width: 100wh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  div {
+    text-align: center;
+  }
+
+</style>
